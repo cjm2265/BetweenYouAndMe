6acbeac6cdf145072b2cc5d88f23ac98057f94e4
cjm2265 <36719420+cjm2265@users.noreply.github.com>
2018-09-16 14:58:29 -0400

    Merge branch 'master' of https://github.com/cjm2265/BetweenYouAndMe
    I hope i'm not overwriting anything

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
-const config = require("./config/config")
-const distanceHelper = require("./helpers/distanceHelper")
-
-const GooglePlaces = require("googleplaces");
-
-const portNumber = 8080
-const htmlPath = __dirname + "/frontend/html/"
-
-const places = new GooglePlaces(config.placesApi.apiKey, config.placesApi.outputFormat);
+var portNumber = 8080
+var htmlPath = __dirname + "/frontend/html/"
 
 //set up app in current directory
 app.use("/js", express.static(__dirname + "/frontend/js"));
@@ -18,7 +11,6 @@ app.use("/resources", express.static(__dirname + "/frontend/resources"));
 app.use("/css", express.static(__dirname + "/frontend/css"));
 app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist/css/"))
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/", function(req, res){
 	res.sendFile(htmlPath + "index.html");
@@ -34,20 +26,6 @@ app.get("/matches-screen", (req, res) => {
 
 app.get("/places-swipe-screen", (req, res) => {
     res.sendFile(htmlPath + "places-swipe-screen.html")
-});
-
-app.post("/distance-between", (req, res) => {
-    res.json({distance: distanceBetween(req.body.pointA, req.body.pointB)});
-})
-
-app.post("/places-between", (req, res) => {
-    let pointA = req.body.pointA;
-    let pointB = req.body.pointB;
-    let parameters = distanceHelper.parameters(pointA, pointB)
-
-    places.placeSearch(parameters, (err, result) =>{
-        res.json(result);
-    });
 })
 
 
--- config/config-template.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    placesApi: {
-        apiKey: 'Enter Maps API KEY here',
-        outputFormat: 'json'
-    }
-}
